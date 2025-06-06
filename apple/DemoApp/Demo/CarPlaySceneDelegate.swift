1095a623ae701e0553e403b2f88a72ac8d93969d
Greg Bolsinga <bolsinga@mac.com>
2025-06-06 08:07:41 -0700

    iOS: Update DemoApp CarPlay Support
    - Add `DemoCarPlayModel`. It has a `DemoModel` and the CarPlay types.
    - Update the `CarPlayDelegate` to use the `DemoCarPlayModel`.
    - The SwiftUI Views in the application use `@Observable` properties to populate the SwiftUI views.
    - No longer uses the old code path; but that is currently kept for backwards compatibility.

--- apple/DemoApp/Demo/CarPlaySceneDelegate.swift
+++ apple/DemoApp/Demo/CarPlaySceneDelegate.swift
@@ -10,18 +10,81 @@ private extension Logger {
     static let carPlay = Logger(subsystem: "ferrostar", category: "carplaydelegate")
 }
 
-private let CarPlaySceneDelegateKey = "ferrostar"
+private let ModelKey = "com.stadiamaps.ferrostar.model"
 
 private extension UISceneSession {
-    var carPlayManager: FerrostarCarPlayManager? {
+    var model: DemoCarPlayModel? {
         get {
-            userInfo?[CarPlaySceneDelegateKey] as? FerrostarCarPlayManager
+            userInfo?[ModelKey] as? DemoCarPlayModel
         }
         set {
             var info = userInfo ?? [:]
-            info[CarPlaySceneDelegateKey] = newValue
+            info[ModelKey] = newValue
             userInfo = info
         }
+    }
+}
+
+@MainActor
+private extension CPBarButton {
+    convenience init(appState: DemoAppState, model: DemoCarPlayModel, mapTemplate: CPMapTemplate) {
+        self.init(title: appState.buttonText, handler: appState.handler(model, mapTemplate: mapTemplate))
+    }
+
+    convenience init(model: DemoCarPlayModel, mapTemplate: CPMapTemplate) {
+        let appState = model.appState
+        self.init(appState: appState, model: model, mapTemplate: mapTemplate)
+    }
+}
+
+@MainActor
+private extension DemoAppState {
+    func handler(_ model: DemoCarPlayModel, mapTemplate: CPMapTemplate) -> CPBarButtonHandler {
+        { _ in
+            Logger.carPlay.debug("\(self.buttonText, privacy: .public)")
+            switch self {
+            case .idle:
+                model.chooseDestination()
+                model.updateButtons(mapTemplate)
+            case let .destination(coordinate):
+                Task {
+                    await model.loadRoute(coordinate)
+                    model.updateButtons(mapTemplate)
+                }
+            case let .routes(routes):
+                model.preview(routes, mapTemplate: mapTemplate)
+                model.updateButtons(mapTemplate)
+            case let .selectedRoute(route):
+                model.startNavigationSession(route, mapTemplate: mapTemplate)
+                model.updateButtons(mapTemplate)
+            case .navigating:
+                model.stop()
+                model.updateButtons(mapTemplate)
+            }
+        }
+    }
+}
+
+private extension DemoCarPlayModel {
+    private func leadingNavigationBarButtons(_ mapTemplate: CPMapTemplate) -> [CPBarButton] {
+        if case .navigating = appState {
+            []
+        } else {
+            [CPBarButton(model: self, mapTemplate: mapTemplate)]
+        }
+    }
+
+    private func trailingNavigationBarButtons(_ mapTemplate: CPMapTemplate) -> [CPBarButton] {
+        if case .idle = appState {
+            []
+        } else {
+            [CPBarButton(appState: .navigating, model: self, mapTemplate: mapTemplate)]
+        }
+    }
+
+    func updateButtons(_ mapTemplate: CPMapTemplate) {
+        mapTemplate.leadingNavigationBarButtons = leadingNavigationBarButtons(mapTemplate)
+        mapTemplate.trailingNavigationBarButtons = trailingNavigationBarButtons(mapTemplate)
     }
 }
 
@@ -33,108 +96,53 @@ class CarPlaySceneDelegate: NSObject, CPTemplateApplicationSceneDelegate {
     ) {
         Logger.carPlay.debug("\(#function)")
 
-        guard templateApplicationScene.session.carPlayManager == nil else {
+        guard templateApplicationScene.session.model == nil else {
             Logger.carPlay.error("CarPlay already connected?")
             return
         }
-
         guard let model = demoModel else {
             Logger.carPlay.error("No shared DemoModel")
             return
         }
 
-        let manager = setupCarPlay(on: window, model: model)
-        let mapTemplate = manager.mapTemplate
+        let carPlayModel = DemoCarPlayModel(model: model)
+        templateApplicationScene.session.model = carPlayModel
 
-        // Set the root template
-        interfaceController.setRootTemplate(mapTemplate, animated: true) { success, error in
-            if let error {
-                Logger.carPlay.error("Failed setRootTemplet: \(error)")
-            } else {
-                Logger.carPlay.debug("Template presented: \(success)")
+        let view = DemoCarPlayView(model: carPlayModel)
+
+        let vc = UIHostingController(rootView: view)
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+
+        let mapTemplate = CPMapTemplate()
+        mapTemplate.mapDelegate = carPlayModel
+        carPlayModel?.updateButtons(mapTemplate)
+
+        Task { @MainActor in
+            do {
+                _ = try await interfaceController.setRootTemplate(mapTemplate, animated: true)
+            } catch {
+                Logger.carPlay.error("Cannot setRootTemplate")
+                carPlayModel?.errorMessage = error.localizedDescription
             }
         }
-
-        templateApplicationScene.session.carPlayManager = manager
     }
 
     public func templateApplicationScene(
         _ templateApplicationScene: CPTemplateApplicationScene,
         didDisconnect _: CPInterfaceController,
-        from _: CPWindow
+        from window: CPWindow
     ) {
         Logger.carPlay.debug("\(#function)")
 
-        guard let manager = templateApplicationScene.session.carPlayManager else {
+        guard let model = templateApplicationScene.session.model else {
             Logger.carPlay.error("CarPlay not connected?")
             return
         }
 
-        manager.disconnect()
+        model.stop()
+        window.isHidden = true
 
-        templateApplicationScene.session.carPlayManager = nil
+        templateApplicationScene.session.model = nil
     }
-
-    private func setupCarPlay(on window: UIWindow, model: DemoModel) -> FerrostarCarPlayManager {
-        let view = DemoCarPlayNavigationView(model: model, styleURL: AppDefaults.mapStyleURL)
-
-        let carPlayViewController = UIHostingController(rootView: view)
-
-        let carPlayManager = FerrostarCarPlayManager(
-            model.core,
-            distanceUnits: .default,
-            // TODO: We may need to hold the view or viewController here, but it seems
-            //       to work for now.
-            showCentering: !model.camera.isTrackingUserLocationWithCourse,
-            onCenter: { model.camera = .automotiveNavigation(pitch: 25)
-            },
-            onStartTrip: {
-                // TODO: This will require some work on the FerrostarCore side - to accept a route before starting.
-            },
-            onCancelTrip: {
-                model.core.stopNavigation()
-            }
-        )
-
-        window.rootViewController = carPlayViewController
-        window.makeKeyAndVisible()
-
-        return carPlayManager
-    }
-}
-
-extension CarPlaySceneDelegate: CPTemplateApplicationDashboardSceneDelegate {
-    func templateApplicationDashboardScene(
-        _: CPTemplateApplicationDashboardScene,
-        didConnect _: CPDashboardController,
-        to _: UIWindow
-    ) {
-        Logger.carPlay.info("\(#function)")
-    }
-
-    func templateApplicationDashboardScene(
-        _: CPTemplateApplicationDashboardScene,
-        didDisconnect _: CPDashboardController,
-        from _: UIWindow
-    ) {
-        Logger.carPlay.info("\(#function)")
-    }
-}
-
-extension CarPlaySceneDelegate: CPTemplateApplicationInstrumentClusterSceneDelegate {
-    // swiftlint:disable identifier_name vertical_parameter_alignment
-    func templateApplicationInstrumentClusterScene(
-        _: CPTemplateApplicationInstrumentClusterScene,
-        didConnect _: CPInstrumentClusterController
-    ) {
-        Logger.carPlay.info("\(#function)")
-    }
-
-    func templateApplicationInstrumentClusterScene(
-        _: CPTemplateApplicationInstrumentClusterScene,
-        didDisconnectInstrumentClusterController _: CPInstrumentClusterController
-    ) {
-        Logger.carPlay.info("\(#function)")
-    }
-    // swiftlint:enable identifier_name vertical_parameter_alignment
 }
---
+++ apple/DemoApp/Demo/DemoCarPlayModel.swift
@@ -0,0 +1,124 @@
+@preconcurrency import CarPlay
+import FerrostarCarPlayUI
+import FerrostarCore
+import FerrostarCoreFFI
+import FerrostarSwiftUI
+import Foundation
+
+@MainActor
+@Observable final class DemoCarPlayModel: NSObject, @preconcurrency CPMapTemplateDelegate {
+    var model: DemoModel
+    var session: CPNavigationSession?
+
+    let formatterCollection: FormatterCollection = FoundationFormatterCollection()
+
+    init?(model: DemoModel?) {
+        guard let model else { return nil }
+        self.model = model
+    }
+
+    var appState: DemoAppState { model.appState }
+    var errorMessage: String? {
+        get {
+            model.errorMessage
+        }
+        set {
+            model.errorMessage = newValue
+        }
+    }
+
+    var core: FerrostarCore { model.core }
+
+    func chooseDestination() {
+        model.chooseDestination()
+    }
+
+    func loadRoute(_ destination: CLLocationCoordinate2D) async {
+        await model.loadRoute(destination)
+    }
+
+    func chooseRoute(_ route: Route) {
+        model.chooseRoute(route)
+    }
+
+    func navigate(_ route: Route) {
+        model.navigate(route)
+    }
+
+    func stop() {
+        session?.cancelTrip()
+        model.stop()
+    }
+
+    private func start(choice: CPRouteChoice, mapTemplate: CPMapTemplate) {
+        do {
+            guard let route = choice.route else { throw DemoError.invalidCPRouteChoice }
+            startNavigationSession(route, mapTemplate: mapTemplate)
+        } catch {
+            model.errorMessage = error.localizedDescription
+            model.appState = .idle
+        }
+    }
+
+    private func select(choice: CPRouteChoice) {
+        do {
+            guard let route = choice.route else { throw DemoError.invalidCPRouteChoice }
+            model.chooseRoute(route)
+        } catch {
+            model.errorMessage = error.localizedDescription
+            model.appState = .idle
+        }
+    }
+
+    private func trackCoreChanges(_ mapTemplate: CPMapTemplate, units: MKDistanceFormatter.Units = .default) {
+        withObservationTracking {
+            guard let session else { return }
+            guard let state = model.coreState else { return }
+
+            state.updateEstimates(mapTemplate: mapTemplate, session: session, units: units)
+        } onChange: {
+            Task { @MainActor in
+                self.trackCoreChanges(mapTemplate, units: units)
+            }
+        }
+    }
+
+    private var start: MKMapItem { MKMapItem(placemark: MKPlacemark(coordinate: model.origin)) }
+    private var end: MKMapItem { MKMapItem(placemark: MKPlacemark(coordinate: model.destination)) }
+
+    func trip(_ routes: [Route]) -> CPTrip {
+        CPTrip.fromFerrostar(
+            routes: routes,
+            origin: start,
+            destination: end,
+            distanceFormatter: formatterCollection.distanceFormatter,
+            durationFormatter: formatterCollection.durationFormatter
+        )
+    }
+
+    func startNavigationSession(_ route: Route, mapTemplate: CPMapTemplate) {
+        let trip = trip([route])
+        session = mapTemplate.startNavigationSession(for: trip)
+        navigate(route)
+        trackCoreChanges(mapTemplate)
+    }
+
+    func preview(_ routes: [Route], mapTemplate: CPMapTemplate) {
+        let trip = trip(routes)
+        mapTemplate.showRouteChoicesPreview(for: trip, textConfiguration: nil)
+    }
+
+    func mapTemplate(_: CPMapTemplate, selectedPreviewFor _: CPTrip, using routeChoice: CPRouteChoice) {
+        // FIXME: Show route on the map.
+        select(choice: routeChoice)
+    }
+
+    func mapTemplate(_ mapTemplate: CPMapTemplate, startedTrip _: CPTrip, using routeChoice: CPRouteChoice) {
+        start(choice: routeChoice, mapTemplate: mapTemplate)
+        mapTemplate.hideTripPreviews()
+    }
+
+    func mapTemplateDidCancelNavigation(_: CPMapTemplate) {
+        stop()
+    }
+}
